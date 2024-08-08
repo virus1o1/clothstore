e3ea2e71a81d7a9500ee0114e671445e9f0ed145
siamtalukder <sium15-13645@diu.edu.bd>
2024-08-08 21:43:51 +0600

    added new features

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./Home.css";
 import Banner from "../Banner/Banner";
 import Products from "../Showcase/Products";
+import { FaShippingFast, FaRedo, FaFingerprint } from "react-icons/fa";
+import { MdOutlineSupport } from "react-icons/md";
 
 const Home = () => {
   return (
@@ -13,7 +15,44 @@ const Home = () => {
           <p>recent added shirts!</p>
         </div>
         <Products />
-        <Products />
+        <ul className="list-unstyled d-flex flex-wrap justify-content-between mt-4 mb-4">
+          <li className="d-flex align-items-start col-lg-3 col-md-6 mb-4 p-3">
+            <FaShippingFast size={30} className="icon-color me-3" />
+            <div>
+              <h5 className="title mb-1">FREE SHIPPING</h5>
+              <p className="text-color mb-0 pe-5">
+                Enjoy free shipping on all orders above $100
+              </p>
+            </div>
+          </li>
+          <li className="d-flex align-items-start col-lg-3 col-md-6 mb-4 p-3">
+            <MdOutlineSupport size={30} className="icon-color me-3" />
+            <div>
+              <h5 className="title mb-1">SUPPORT 24/7</h5>
+              <p className="text-color mb-0 pe-5">
+                Our support team is there to help you for queries
+              </p>
+            </div>
+          </li>
+          <li className="d-flex align-items-start col-lg-3 col-md-6 mb-4 p-3">
+            <FaRedo size={30} className="icon-color me-3" />
+            <div>
+              <h5 className="title mb-1">30 DAYS RETURN</h5>
+              <p className="text-color mb-0 pe-5">
+                Simply return it within 30 days for an exchange
+              </p>
+            </div>
+          </li>
+          <li className="d-flex align-items-start col-lg-3 col-md-6 mb-4 p-3">
+            <FaFingerprint size={30} className="icon-color me-3" />
+            <div>
+              <h5 className="title mb-1">100% PAYMENT SECURE</h5>
+              <p className="text-color mb-0 pe-5">
+                Our payments are secured with 256 bit encryption
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </>
   );
--- src/components/Showcase/Products.js
+++ src/components/Showcase/Products.js
@@ -5,46 +5,44 @@ import show2 from "../../assets/Images/show2.jfif";
 import show3 from "../../assets/Images/show3.jfif";
 import show4 from "../../assets/Images/show4.jfif";
 
+const products = [
+  {
+    img: show1,
+    title: "Plain White Shirt",
+    price: "$29.00"
+  },
+  {
+    img: show2,
+    title: "Denim Jacket",
+    price: "$69.00"
+  },
+  {
+    img: show3,
+    title: "Black Polo Shirt",
+    price: "$49.00"
+  },
+  {
+    img: show4,
+    title: "Blue Sweatshirt",
+    price: "$79.00"
+  }
+];
+
 const Products = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card h-100">
-            <img src={show1} className="card-img-top" alt="Plain White Shirt" />
-            <div className="card-body text-center">
-              <h5 className="card-title fs-6 fw-bold">Plain White Shirt</h5>
-              <p className="card-text">$29.00</p>
+        {products.map((product, index) => (
+          <div key={index} className="col-6 col-sm-6 col-md-6 col-lg-3 mb-4">
+            <div className="card h-100">
+              <img src={product.img} className="card-img-top" alt={product.title} />
+              <div className="card-body text-center">
+                <h5 className="card-title fs-6 fw-bold">{product.title}</h5>
+                <p className="card-text">{product.price}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card h-100">
-            <img src={show2} className="card-img-top" alt="Denim Jacket" />
-            <div className="card-body text-center">
-              <h5 className="card-title fs-6 fw-bold">Denim Jacket</h5>
-              <p className="card-text">$69.00</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card h-100">
-            <img src={show3} className="card-img-top" alt="Black Polo Shirt" />
-            <div className="card-body text-center">
-              <h5 className="card-title fs-6 fw-bold">Black Polo Shirt</h5>
-              <p className="card-text">$49.00</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card h-100">
-            <img src={show4} className="card-img-top" alt="Blue Sweatshirt" />
-            <div className="card-body text-center">
-              <h5 className="card-title fs-6 fw-bold">Blue Sweatshirt</h5>
-              <p className="card-text">$79.00</p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
